3a0f276be0b46d6644f97252f7649bed19297256
Vladislav Naftolin <vnaftolin@mail.ru>
2025-01-02 12:45:04 +0300

    feat: implement countCats function

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,9 +14,14 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats(matrix) {
+  return matrix.reduce((res, item) => {
+    res += item.reduce((catsCounter, elem) => {
+      if (elem === '^^') return ++catsCounter;
+      return catsCounter;
+    }, 0);
+    return res;
+  }, 0)
 }
 
 module.exports = {
